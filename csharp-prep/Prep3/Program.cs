1326dc7047850046130d4de4e5e8d5b6d457e476
eeshurtliff <eeshurtliff@gmail.com>
2023-09-22 11:47:18 -0600

    finished the core requirements for prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Concurrent;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        int eesCorrect = 0;
+        Random randomGenerator = new Random();
+        int eesMagicNumber = randomGenerator.Next(1,11);
+        
+        do {
+            Console.Write("What is your guess? ");
+            string eesUserGuess = Console.ReadLine();
+            int eesGuess = int.Parse(eesUserGuess);
+            if (eesGuess < eesMagicNumber){
+                Console.WriteLine("Higher");
+            }
+            else if (eesGuess > eesMagicNumber){
+                Console.WriteLine("Lower");
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+                eesCorrect = 1;
+            } 
+
+        } while (eesCorrect == 0);
     }
 }
