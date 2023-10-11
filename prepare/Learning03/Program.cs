cd361269c4a819b74982f6cfc9724c8f65d285f4
eeshurtliff <eeshurtliff@gmail.com>
2023-10-10 22:15:21 -0600

    completed learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6,7);
+        Console.WriteLine(fraction1.GetBottom());
+
+
+        Fraction test1 = new Fraction(1);
+        Console.WriteLine(test1.GetFractionString());
+        Console.WriteLine(test1.GetDecimalValue());
+
+        Fraction test2 = new Fraction(5);
+        Console.WriteLine(test2.GetFractionString());
+        Console.WriteLine(test2.GetDecimalValue());
+
+
+        Fraction test3 = new Fraction(3,4);
+        Console.WriteLine(test3.GetFractionString());
+        Console.WriteLine(test3.GetDecimalValue());
+
+        Fraction test4 = new Fraction(1,3);
+        Console.WriteLine(test4.GetFractionString());
+        Console.WriteLine(test4.GetDecimalValue());
+
     }
 }
