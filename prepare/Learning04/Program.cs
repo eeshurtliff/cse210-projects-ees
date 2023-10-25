3c4d14763550d749e5263c8bc9d205513dc3385d
eeshurtliff <eeshurtliff@gmail.com>
2023-10-25 09:59:44 -0600

    finished learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssignment = new Assignment("Joseph", "Fractions");
+        // Console.WriteLine(newAssignment.GetSummary());
+        MathAssignment newMath = new MathAssignment("Kolton", "Division", "2.3", "All Odd");
+        // Console.WriteLine(newMath.GetSummary());
+        Console.WriteLine(newMath.GetHomework());
+        Console.WriteLine();
+        WritingAssignment writing = new WritingAssignment("Zach", "Poems", "The Importance of Poetry");
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
